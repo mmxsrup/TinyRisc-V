2687ce0d0bfa7d542abd22d2af6ef64b3ad1a206
srup <sugiyama98i@gmail.com>
2019-01-02 23:55:45 +0900

    created interface to connect with ram(main memory)

--- core/main/dcache.v
+++ core/main/dcache.v
@@ -1,10 +1,13 @@
 module dcache #( // data cache
-	parameter CACHE_SIZE = 4096
+	parameter CACHE_SIZE = 4096,
+	parameter DWIDTH = 32,
+	parameter AWIDTH = 32,
+	parameter LWIDTH = 2
 )(
 	input clk,
 	input rst,
 	
-	// from lsu
+	// from memory
 	input [31 : 0] addr,
 	input wreq, // write request
 	input rreq, // read request
@@ -14,19 +17,38 @@ module dcache #( // data cache
 	// to controller
 	output wvalid,
 	output reg [31 : 0] rdata,
-	output rvalid
+	output rvalid,
 
-	// from memory
-	// TODO
+	// from ram and to ram
+	// write
+	output reg [AWIDTH - 1 : 0] ram_awaddr,
+	output reg [LWIDTH - 1 : 0] ram_awlen,
+	output reg ram_awvalid,
+	input ram_awready,
+	output reg [DWIDTH - 1 : 0] ram_wdata,
+	input ram_wvalid,
+	output reg ram_wready,
+	input ram_wlast,
+	// read
+	output reg [AWIDTH - 1 : 0] ram_araddr,
+	output reg [LWIDTH - 1 : 0] ram_arlen,
+	output reg ram_arvalid,
+	input ram_arready,
+	input [DWIDTH - 1 : 0] ram_rdata,
+	input ram_rvalid,
+	output reg ram_rready,
+	input ram_rlast
 );
 	
-	parameter STATE_SIZE = 3;
-	parameter IDLE = 3'h1;
-	parameter RUN  = 3'h2;
-	parameter WRITE_BACK = 3'h3;
-	parameter ALLOCATE = 3'h4;
+	localparam SWIDTH = 3;
+	localparam IDLE = 3'h0;
+	localparam WRITE_BACK = 3'h1;
+	localparam WWAIT = 3'h2;
+	localparam ALLOCATE = 3'h3;
+	localparam RWAIT = 3'h4;
 
-	reg [STATE_SIZE - 1 : 0] state;
+	reg [SWIDTH - 1 : 0] state;
+
 
 	reg v [0 : CACHE_SIZE - 1]; // valid
 	reg d [0 : CACHE_SIZE - 1]; // dirty bit
@@ -38,20 +60,19 @@ module dcache #( // data cache
 	wire [11 : 0] addr_index;
 	wire hit;
 
+	integer i = 0;
 	initial begin
-		for (integer i = 0; i < CACHE_SIZE; i = i + 1) begin
-			v[i] = 1;
+		for (i = 0; i < CACHE_SIZE; i = i + 1) begin
+			v[i] = 0;
 			d[i] = 0;
 			tag[i] = 0;
-			cache_data[i] = i;
+			cache_data[i] = 0;
 		end
-		// TODO
-		// $readmemh("dcache_data.hex", MEM);
 	end
 
 	assign addr_tag = addr[31 : 14];
 	assign addr_index = addr[13 : 2];
-	assign hit = (tag[addr_index] == addr_tag && v[addr_index]) ? 1 : 0;
+	assign hit = (tag[addr_index] == addr_tag && v[addr_index]);
 
 	assign wvalid = (wreq && hit) ? 1 : 0;
 	assign rvalid = (rreq && hit) ? 1 : 0;
@@ -70,45 +91,113 @@ module dcache #( // data cache
 	end
 
 	always @(posedge clk) begin
-		if (wreq) begin
-			if (byte_enable[0]) cache_data[addr_index][7 : 0]	 <= wdata[7 : 0];
-			if (byte_enable[1]) cache_data[addr_index][15 : 8]	 <= wdata[15 : 8];
-			if (byte_enable[2]) cache_data[addr_index][23 : 16]	 <= wdata[23 : 16];
-			if (byte_enable[3]) cache_data[addr_index][31 : 24]	 <= wdata[31 : 24];
+		if (hit && wreq) begin
+			case (addr[1 : 0])
+				2'b00 : begin
+					if (byte_enable[0]) cache_data[addr_index][ 7 :  0] <= wdata[7  :  0];
+					if (byte_enable[1]) cache_data[addr_index][15 :  8]	 <= wdata[15 :  8];
+					if (byte_enable[2]) cache_data[addr_index][23 : 16]	 <= wdata[23 : 16];
+					if (byte_enable[3]) cache_data[addr_index][31 : 24]	 <= wdata[31 : 24];
+				end
+				2'b01 : begin
+					if (byte_enable[0]) cache_data[addr_index    ][15 :  8]	 <= wdata[ 7 :  0];
+					if (byte_enable[1]) cache_data[addr_index    ][23 : 16]	 <= wdata[15 :  8];
+					if (byte_enable[2]) cache_data[addr_index    ][31 : 24]	 <= wdata[23 : 16];
+					if (byte_enable[3]) cache_data[addr_index + 1][ 7 :  0]	 <= wdata[31 : 24];
+				end
+				2'b10 : begin
+					if (byte_enable[0]) cache_data[addr_index    ][23 : 16]	 <= wdata[ 7 :  0];
+					if (byte_enable[1]) cache_data[addr_index    ][31 : 24]	 <= wdata[15 :  8];
+					if (byte_enable[2]) cache_data[addr_index + 1][ 7 :  0]	 <= wdata[23 : 16];
+					if (byte_enable[3]) cache_data[addr_index + 1][15 :  8]	 <= wdata[31 : 24];
+				end
+				2'b11 : begin
+					if (byte_enable[0]) cache_data[addr_index    ][31 : 24]	 <= wdata[ 7 :  0];
+					if (byte_enable[1]) cache_data[addr_index + 1][ 7 :  0]	 <= wdata[15 :  8];
+					if (byte_enable[2]) cache_data[addr_index + 1][15 :  8]	 <= wdata[23 : 16];
+					if (byte_enable[3]) cache_data[addr_index + 1][23 : 16]	 <= wdata[31 : 24];
+				end
+			endcase // (addr[1 : 0])
 		end
 	end
 
+	integer j;
 	always @(posedge clk) begin
 		if (rst) begin
-			for (integer i = 0; i < CACHE_SIZE; i = i + 1) begin
-				d[i] <= 0;
+			for (j = 0; j < CACHE_SIZE; j = j + 1) begin
+				d[j] <= 0;
 			end
 		end else begin
 			if (hit && wreq) d[addr_index] <= 1;
 		end
 	end
 
-	// state machine
+
+	reg [2 : 0] wcnt_done;
+	reg [2 : 0] rcnt_done;
+
 	always @(posedge clk) begin
 		if (rst) begin
 			state <= IDLE;
+			ram_awaddr <= 32'h0;
+			ram_awlen  <= 0;
+			ram_awvalid <= 1'b0;
+			ram_wdata <= 32'h0;
+			ram_wready <= 1'b0;
+			ram_araddr <= 32'b0;
+			ram_arlen  <= 0;
+			ram_arvalid <= 1'b0;
+			ram_rready <= 1'b0;
+			wcnt_done <= 0;
+			rcnt_done <= 0;
 		end else begin
 
 			case (state)
-				IDLE : state <= RUN;
-				RUN : begin
-					if (wreq || rreq) begin // wrtie or read request
-						if (hit) state <= RUN;
-						else begin
+				IDLE : begin
+					ram_rready <= 0;
+					wcnt_done <= 0;
+					rcnt_done <= 0;
+					if (!hit) begin // cache miss
+						if (wreq || rreq) begin
 							if (d[addr_index]) state <= WRITE_BACK;
 							else state <= ALLOCATE;
 						end
-					end else begin // not request
-						state <= RUN;
 					end
 				end
-				WRITE_BACK : state <= WRITE_BACK; // TODO
-				ALLOCATE : state <= ALLOCATE; // TODO
+				WRITE_BACK : begin
+					ram_awaddr <= {tag[addr_index], addr_index, 2'b00}; // write back addr
+					ram_awlen  <= (addr[1 : 0] == 2'b00) ? 1 : 2;
+					ram_awvalid <= 1;
+					ram_wready <= 1;
+					if (ram_awready) state <= WWAIT;
+				end
+				WWAIT : begin
+					ram_awvalid <= 0;
+					if (ram_wvalid) begin
+						ram_wdata <= cache_data[addr_index + wcnt_done]; // write back data
+						wcnt_done <= wcnt_done + 1;
+						if (ram_wlast) state <= ALLOCATE;
+					end
+				end
+				ALLOCATE : begin
+					ram_wready <= 0;
+					ram_araddr <= addr;
+					ram_arlen  <= (addr[1 : 0] == 2'b00) ? 1 : 2;
+					ram_arvalid <= 1;
+					ram_rready <= 1;
+					if (ram_arready) state <= RWAIT;
+				end
+				RWAIT : begin
+					ram_arvalid <= 0;
+					if (ram_rvalid) begin
+						cache_data[addr_index + rcnt_done] <= ram_rdata;
+						tag[addr_index + rcnt_done] <= addr_tag;
+						v[addr_index + rcnt_done] <= 1;
+						d[addr_index + rcnt_done] <= 0;
+						rcnt_done <= rcnt_done + 1;
+						if (ram_rlast) state <= IDLE;
+					end
+				end
 				default : state <= IDLE;
 			endcase // case (state)
 
--- core/main/icache.v
+++ core/main/icache.v
@@ -1,5 +1,8 @@
 module icache #(
-	parameter CACHE_SIZE = 4096
+	parameter CACHE_SIZE = 4096,
+	parameter DWIDTH = 32,
+	parameter AWIDTH = 32,
+	parameter LWIDTH = 2
 ) (
 	input clk,
 	input rst,
@@ -9,19 +12,37 @@ module icache #(
 	input req,
 	// to fetch
 	output [31 : 0] data,
-	output valid
+	output valid,
 
-	// from main memory
-	// TODO
+	// from ram and to ram
+	// write
+	output reg [AWIDTH - 1 : 0] ram_awaddr,
+	output reg [LWIDTH - 1 : 0] ram_awlen,
+	output reg ram_awvalid,
+	input ram_awready,
+	output reg [DWIDTH - 1 : 0] ram_wdata,
+	input ram_wvalid,
+	output reg ram_wready,
+	input ram_wlast,
+	// read
+	output reg [AWIDTH - 1 : 0] ram_araddr,
+	output reg [LWIDTH - 1 : 0] ram_arlen,
+	output reg ram_arvalid,
+	input ram_arready,
+	input [DWIDTH - 1 : 0] ram_rdata,
+	input ram_rvalid,
+	output reg ram_rready,
+	input ram_rlast
 );
 	
-	parameter STATE_SIZE = 2;
-	parameter IDLE = 2'b00;
-	parameter RUN  = 2'b01;
-	parameter MISS = 2'b10;
+	parameter SWIDTH = 2;
+	parameter IDLE = 2'h0;
+	parameter ALLOCATE = 2'h1;
+	parameter RWAIT = 2'h2;
 
-	reg [STATE_SIZE - 1 : 0] state;
+	reg [SWIDTH - 1 : 0] state;
 
+	reg v [0 : CACHE_SIZE - 1];
 	reg [11 : 0] tag [0 : CACHE_SIZE - 1];
 	reg [31 : 0] cache_data [0 : CACHE_SIZE - 1];
 
@@ -31,48 +52,64 @@ module icache #(
 
 	initial begin
 		for (integer i = 0; i < CACHE_SIZE; i = i + 1) begin
+			v[i] = 0;
 			tag[i] = 0;
-			cache_data[i] = i;
 		end
-		// TODO
-		// $readmemh("icache_data.hex", MEM);
 	end
 
 	assign addr_tag = addr[31 : 14];
 	assign addr_index = addr[13 : 2];
-	assign hit = (tag[addr_index] == addr_tag);
+	assign hit = (tag[addr_index] == addr_tag && v[addr_index]);
 
 	assign valid = hit;
 	assign data = (hit) ? cache_data[addr_index] : 32'b0;
 
-	// state machine
+
 	always @(posedge clk) begin
 		if (rst) begin
 			state <= IDLE;
+			ram_awaddr <= 32'h0;
+			ram_awlen  <= 0;
+			ram_awvalid <= 1'b0;
+			ram_wdata <= 32'h0;
+			ram_wready <= 1'b0;
+			ram_araddr <= 32'b0;
+			ram_arlen  <= 0;
+			ram_arvalid <= 1'b0;
+			ram_rready <= 1'b0;
 		end else begin
 
 			case (state)
-				IDLE : state <= RUN;
-				RUN  : begin
-					if (req) begin // read request
-						if (hit) begin // HIT
-							state <= RUN;
-						end else begin // MISS
-							state <= MISS;
+				IDLE : begin
+					ram_rready <= 0;
+					if (!hit) begin // cache miss
+						if (req) begin
+							state <= ALLOCATE;
 						end
-					end else begin
-						state <= RUN;
 					end
 				end
-				MISS : begin // TODO
-					state <= MISS;
+				ALLOCATE : begin
+					ram_wready <= 0;
+					ram_araddr <= addr;
+					ram_arlen  <= 1;
+					ram_arvalid <= 1;
+					ram_rready <= 1;
+					if (ram_arready) state <= RWAIT;
 				end
-				default : begin
-					state <= IDLE;
+				RWAIT : begin
+					ram_arvalid <= 0;
+					if (ram_rvalid) begin
+						cache_data[addr_index] <= ram_rdata;
+						tag[addr_index] <= addr_tag;
+						v[addr_index] <= 1;
+						if (ram_rlast) state <= IDLE;
+					end
 				end
+				default : state <= IDLE;
 			endcase // case (state)
 
 		end
 	end // always @(posedge clk)
+
 
 endmodule // icache
